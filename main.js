4da375adf935ac7eae92d426e10755749135a20d
tweesiee <31532449+tweesiee@users.noreply.github.com>
2017-09-20 15:51:20 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("fotoSkit");
     for (i = 0; i < x.length; i++) {
-       //x[i].style.display = "none";
-       $(x[i]).fadeIn();  
+       x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].fadeOut();
-    //x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 4000);
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
 }
 
 $(function() {
